0f30a8c446be2fd11b5d4ffa3d29d54ea00f0238
Hersh Patel <phersh05@gmail.com>
2019-01-16 17:12:21 -0500

    flask-cors

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import json
 from flask import Flask
 from flask import jsonify
 from flask import make_response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/seattle")
 def getSeattleCluster():
